40631a1e1a98b2856c570257ecfeda1225398a5f
Rafaelmaiden <rafaelmaggot64@gmail.com>
2021-02-16 13:32:48 -0300

    Finishes project

--- frontend/src/views/ListProducts.vue
+++ frontend/src/views/ListProducts.vue
@@ -1,182 +1,194 @@
 <template>
-  <b-container class="contain">
-    <b-row class="mt-1  ">
-      <b-col
-        md="7"
-        class="text-right pl-5 pr-0 mb-5"
+  <div>
+  <!-- Loader while loading carda -->
+    <loader v-if="loade"/>
+    <b-container v-else class="contain">
+      <b-row class="mt-1  ">
+        <b-col
+          md="7"
+          class="text-right pl-5 pr-0 mb-5"
+        >
+          <h4> Lista de Produtos</h4>
+        </b-col>
+        <b-col
+          md="5"
+          class="text-right pt-1">
+          <b-row class="ml-5">
+            <b-col md="6" class="text-right mr-0 pr-0">
+              <b-form-checkbox
+                class="mt-1"
+                v-model="editProducts"
+                switch
+              >
+              </b-form-checkbox>
+            </b-col>
+            <b-col md="6" class="text-left ml-0 pl-0 text-edit"><span>Modo edição</span></b-col>
+          </b-row>
+        </b-col>
+      </b-row>
+      <!-- Cards products list -->
+      <b-row
+        align-h="center"
+        class="pl-5 pr-5"
+        no-gutters
       >
-        <h4> Lista de Produtos</h4>
-      </b-col>
-      <b-col
-        md="5"
-        class="text-right"
+        <b-col
+          class="mr-auto ml-auto text-center p-1"
+          v-for="(product, index) in products"
+          :key="index"
+          md="4"
+        >
+          <b-card
+            tag="article"
+            class="mb-2 product-card text-center"
+          >
+            <b-card-group deck class="text-center">
+              <b-button
+                @click="getProductById(product.id)"
+                v-b-modal.modal-delete
+                v-if="editProducts"
+                variant="danger"
+                class="delete-button"
+                v-b-tooltip
+                title="Excluir">
+                <b-icon
+                  icon="x-square"
+                  scale="1.2"
+                  variant="light"
+                ></b-icon>
+              </b-button>
+              <b-card-img  width="20%" src="https://picsum.photos/400/400/?image=20" alt="Image" class="rounded-0 pl-5 pr-5 mb-1"></b-card-img>
+              <b-card-text class="mr-auto ml-auto mt-4">
+                <span class="name text-muted">{{ product.name }} </span> <br>
+                <span class="value">R$ {{ product.price }}</span>
+              </b-card-text>
+            </b-card-group>
+          </b-card>
+        </b-col>
+      </b-row>
+      <!-- Modal of delete product -->
+      <b-modal
+        id="modal-delete"
+        ref="modal"
+        centered
+        hide-footer
+        hide-header
+        no-close-on-backdrop
       >
-        <b-form-checkbox
-          v-model="editProducts"
-          switch
-        >
-          <span class="text-edit">Modo edição</span>
-        </b-form-checkbox>
-      </b-col>
-    </b-row>
-    <!-- Cards products list -->
-    <b-row
-      align-h="center"
-      class="pl-5 pr-5"
-      no-gutters
-    >
-      <b-col
-        class="mr-auto ml-auto text-center p-1"
-        v-for="(product, index) in products"
-        :key="index"
-        md="4"
-      >
-        <b-card
-          tag="article"
-          class="mb-2 product-card text-center"
-        >
-          <b-card-group deck class="text-center">
-            <b-button
-              @click="getProductById(product.id)"
-              v-b-modal.modal-delete
-              v-if="editProducts"
-              variant="danger"
-              class="delete-button"
-              v-b-tooltip
-              title="Excluir">
-              <b-icon
-                icon="x-square"
-                scale="1.2"
-                variant="light"
-              ></b-icon>
-            </b-button>
-            <b-card-img width="20%" src="https://picsum.photos/400/400/?image=20" alt="Image" class="rounded-0 pl-5 pr-5 mb-1"></b-card-img>
-            <b-card-text class="mr-auto ml-auto mt-4">
-              <span class="name text-muted">{{ product.name }} </span> <br>
-              <span class="value">R$ {{ product.price }}</span>
-            </b-card-text>
-          </b-card-group>
-        </b-card>
-      </b-col>
-    </b-row>
-    <!-- Modal of delete product -->
-    <b-modal
-      id="modal-delete"
-      ref="modal"
-      centered
-      hide-footer
-      hide-header
-      no-close-on-backdrop
-    >
-      <h4 class="pt-2">Remover produto</h4>
-      <span style="font-size: 14px;">Você realmente deseja remover este produto?</span>
-      <b-col class="text-right mb-2 mt-4">
-        <b-button
-          @click="$bvModal.hide('modal-delete')"
-          class="close-button mr-3"
-          variant="outline-dark"
-        >
-          Manter
-        </b-button>
-        <b-button
-          @click.prevent="deleteProduct()"
-          class="close-button"
-          variant="danger"
-        >
-          Remover
-        </b-button>
-      </b-col>
-    </b-modal>
-     <!-- Add bar to open add modal-->
-    <b-col v-if="editProducts" md="8" class="mr-auto ml-auto">
-      <div class="edit-bar">
-        <b-row>
-          <b-col class="mt-1">
-            Modo de edição ativo
-          </b-col>
-          <b-col >
-            <b-button
-              @click.prevent="changeEditingState()"
-              class="close-button mr-3"
-              variant="outline-light">
-              Fechar
-            </b-button>
-            <b-button
-              variant="success"
-              v-b-modal.modal-add>
-              Adicionar Produto
-            </b-button>
-          </b-col>
-        </b-row>
-      </div>
-    </b-col>
-    <!-- Add modal -->
-    <b-modal
-      id="modal-add"
-      ref="modal"
-      centered
-      hide-footer
-      no-close-on-backdrop
-      title="Adicionar produto">
-      <form ref="form" @submit.stop.prevent="saveProduct()" class="modal-form ml-auto mr-auto">
-        <div class="div-image text-center">
-          <b-icon
-            icon="image"
-            class="img-model ml-auto mr-auto mt-2"
-            style="width: 50px; height: 70px;">
-          </b-icon><br>
-          <span style="font-size: 14px;">Clique em selecionar imagem ou arraste-a aqui</span><br>
-          <label for="imageUrl" style="color: rgb(20, 115, 230);">Selecionar imagem</label>
-          <b-file id="imageUrl" type="file" style="display:none;" v-model="product.imageUrl"/>
+        <h4 class="pt-2">Remover produto</h4>
+        <span style="font-size: 14px;">Você realmente deseja remover este produto?</span>
+        <b-col class="text-right mb-2 mt-4">
+          <b-button
+            @click="$bvModal.hide('modal-delete')"
+            class="close-button mr-3"
+            variant="outline-dark"
+          >
+            Manter
+          </b-button>
+          <b-button
+            @click.prevent="deleteProduct()"
+            class="close-button"
+            variant="danger"
+          >
+            Remover
+          </b-button>
+        </b-col>
+      </b-modal>
+      <!-- Add bar to open add modal-->
+      <b-col v-if="editProducts" md="8" class="mr-auto ml-auto">
+        <div class="edit-bar ">
+          <b-row>
+            <b-col class="mt-1">
+              Modo de edição ativo
+            </b-col>
+            <b-col >
+              <b-button
+                @click.prevent="changeEditingState()"
+                class="close-button mr-3"
+                variant="outline-light">
+                Fechar
+              </b-button>
+              <b-button
+                variant="success"
+                v-b-modal.modal-add>
+                Adicionar Produto
+              </b-button>
+            </b-col>
+          </b-row>
         </div>
-        <b-form-group
-          label="Nome"
-          label-for="name"
-          invalid-feedback="O campo nome é requerido."
-          :state="nameState">
-          <b-form-input
-            class="modal-add-inputs"
-            id="name"
-            v-model="product.name"
-            :state="nameState"
-            required>
-          </b-form-input>
-        </b-form-group>
-        <b-form-group
-          label="Preço"
-          label-for="price"
-          invalid-feedback="O campo preço é requerido."
-          :state="nameState">
-          <b-form-input
-            type="number"
-            class="modal-add-inputs text-right"
-            id="price"
-            v-model="product.price"
-            :state="priceState"
-            required>
-          </b-form-input>
-        </b-form-group>
-        <b-button
-          type="submit"
-          class="modal-add-inputs ml-auto mr-auto"
-          size="md"
-          variant="success"
-          block>
-          Adicionar
-        </b-button>
-      </form>
-    </b-modal>
-  </b-container>
+      </b-col>
+      <!-- Add modal -->
+      <b-modal
+        id="modal-add"
+        ref="modal"
+        centered
+        hide-footer
+        no-close-on-backdrop
+        title="Adicionar produto">
+        <form ref="form" @submit.stop.prevent="saveProduct()" class="modal-form ml-auto mr-auto">
+          <div class="div-image text-center">
+            <b-icon
+              icon="image"
+              class="img-model ml-auto mr-auto mt-2"
+              style="width: 50px; height: 70px;">
+            </b-icon><br>
+            <span style="font-size: 14px;">Clique em selecionar imagem ou arraste-a aqui</span><br>
+            <label for="imageUrl" style="color: rgb(20, 115, 230);">Selecionar imagem</label>
+            <b-file id="imageUrl" type="file" style="display:none;" v-model="product.imageUrl"/>
+          </div>
+          <b-form-group
+            label="Nome"
+            label-for="name"
+            invalid-feedback="O campo nome é requerido."
+            :state="nameState">
+            <b-form-input
+              class="modal-add-inputs"
+              id="name"
+              v-model="product.name"
+              :state="nameState"
+              required>
+            </b-form-input>
+          </b-form-group>
+          <b-form-group
+            label="Preço"
+            label-for="price"
+            invalid-feedback="O campo preço é requerido."
+            :state="nameState">
+            <b-form-input
+              type="number"
+              class="modal-add-inputs text-right"
+              id="price"
+              v-model="product.price"
+              :state="priceState"
+              required>
+            </b-form-input>
+          </b-form-group>
+          <b-button
+            type="submit"
+            class="modal-add-inputs ml-auto mr-auto"
+            size="md"
+            variant="success"
+            block>
+            Adicionar
+          </b-button>
+        </form>
+      </b-modal>
+    </b-container>
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
 import Swal from 'sweetalert2'
+import loader from '../components/loader.vue'
 
 export default {
+  components: { loader },
   name: 'ListProduct',
+  components: { loader },
   data () {
     return {
+      loade: true,
       editProducts: false,
       products: [],
       nameState: null,
@@ -189,16 +201,22 @@ export default {
         price: '',
         image_url: null
       },
-      productIdToDelete: null
+      productIdToDelete: null,
+      ok: false
     }
   },
 
+  created() {
+    this.getProducts()
+  },
   /* props: {
     closeOnEsc: { type: Boolean, default: true }
   }, */
 
-  mounted() {
-    this.getProducts()
+  watch: {
+    ok: function () {
+      if (this.ok) this.loade = false
+    }
   },
 
   methods: {
@@ -211,12 +229,12 @@ export default {
     async getProducts () {
       const get = await axios.get('http://127.0.0.1:5000/products')
       this.products = get.data
+      this.ok = true
     },
 
    async getProductById (id) {
-      const getById = await axios.get('http://127.0.0.1:5000/products')
+      await axios.get('http://127.0.0.1:5000/products')
       this.productIdToDelete = id
-      console.log(this.productIdToDelete)
     },
 
     changeEditingState () {
@@ -245,7 +263,7 @@ export default {
 
     async deleteProduct () {
       await axios.delete('http://127.0.0.1:5000/products/' + this.productIdToDelete)
-      console.log("Produto deletado!")
+      console.log('Produto deletado!')
       this.getProducts()
       this.changeEditingState()
       await Swal.fire({
@@ -286,7 +304,7 @@ export default {
 
 .text-edit {
   font-size: 12px;
-  padding-bottom: 2px;
+  padding-top: 6px !important;
 }
 
 .custom-control-input:checked ~ .custom-control-label::before {
@@ -328,12 +346,15 @@ export default {
 
 /* Stylish for add bar */
 .edit-bar {
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
   padding: 0.75rem !important;
   background: rgba(0, 0, 0, 0.85);
   color: #fff;
   float: center !important;
-  top: -10rem;
-  position: relative;
+  top: 35rem;
+  position: fixed;
 }
 
 .close-button {
